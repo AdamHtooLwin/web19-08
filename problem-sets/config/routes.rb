ddf2998e8a09bde6335ef014a112265751166436
AdamHtooLwin <adamhtoolwin@gmail.com>
2019-09-13 14:15:45 +0700

    Add basics to routes

--- problem-sets/config/routes.rb
+++ problem-sets/config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'ps1/scrapper'
   root 'site#index'
   get 'site/index'
+
+  get 'basics' => 'ps1#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
