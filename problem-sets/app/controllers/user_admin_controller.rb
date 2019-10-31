6e9c1da6b2d54bd85f4a82c60d69e296d78ebe5e
AdamHtooLwin <adamhtoolwin@gmail.com>
2019-10-31 21:09:21 +0700

    Add temporary check for admin/normal user

--- problem-sets/app/controllers/user_admin_controller.rb
+++ problem-sets/app/controllers/user_admin_controller.rb
@@ -1,5 +1,9 @@
 class UserAdminController < ApplicationController
   def index
+    if !current_user or !current_user.is_admin
+      redirect_to site_index_path
+    end
+
     @users = User.all
   end
 end
