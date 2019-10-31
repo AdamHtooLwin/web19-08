3052f21c4f84f1b30066e5ea0b42e28c2f92f481
AdamHtooLwin <adamhtoolwin@gmail.com>
2019-10-31 17:24:45 +0700

    Add ban and is_ban to db row

--- problem-sets/db/schema.rb
+++ problem-sets/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_130855) do
+ActiveRecord::Schema.define(version: 2019_10_31_093612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2019_10_30_130855) do
     t.string "first_name"
     t.string "last_name"
     t.boolean "is_admin"
+    t.boolean "is_banned"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
