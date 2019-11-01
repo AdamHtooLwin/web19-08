09eae8b9a4fb4cbc0dfeac4c70982a6ae206651d
Kiran Dhakal <st121108@ait.asia>
2019-11-01 18:21:52 +0700

    BDD few step definitions for user

--- problem-sets/features/step_definitions/user_steps.rb
+++ problem-sets/features/step_definitions/user_steps.rb
@@ -1,17 +1,22 @@
 Given("I want to register myself in the application") do
-  pending # Write code here that turns the phrase above into concrete actions
+  # pending # Write code here that turns the phrase above into concrete actions
 end
 
 Given("I visit the home page") do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit '/'
 end
 
 Then("I should see the sign up form") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).to have_selector('form.sign_up')
 end
 
 When("I fill up my details in the form and click on “Register” button") do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit '/'
+  fill_in 'sign_up_first_name', with: 'user1.first_name'
+  fill_in 'sign_up_last_name', with: 'user1.last_name'
+  fill_in 'sign_up_email', with: 'user1.email'
+  fill_in 'sign_up_password', with: 'password'
+  click_button 'Sign up'
 end
 
 Then("I should be directed back to home\/login page") do
--- problem-sets/features/user.feature
+++ problem-sets/features/user.feature
@@ -5,7 +5,7 @@ Feature: User
 
   Okay
 
-    Given I want to register myself in the application
+#    Given I want to register myself in the application
     And I visit the home page
     Then I should see the sign up form
     When I fill up my details in the form and click on “Register” button
