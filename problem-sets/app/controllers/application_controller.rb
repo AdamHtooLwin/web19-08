db9d0e30386732330680804ee2bf7240cfd1150f
AdamHtooLwin <adamhtoolwin@gmail.com>
2019-11-01 15:26:39 +0700

    Check for ban/unban

--- problem-sets/app/controllers/application_controller.rb
+++ problem-sets/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :check_banned, except:
+
+  def check_banned
+    if user_signed_in?
+      if current_user.is_banned and request.path != '/users/sign_out'
+        redirect_to site_index_path
+      end
+    end
+  end
 end
